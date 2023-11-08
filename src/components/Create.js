34baa7b01d24a47a26bf448da2c81c474a667fcd
Henk <goldenkingtele@gmail.com>
2023-11-08 22:38:00 +0800

    update create url

--- src/components/Create.js
+++ src/components/Create.js
@@ -21,7 +21,7 @@ export default function Create(){
         // When a post request is sent to the create url, we'll add a new record to the database.
         const newPost = { ...form };
      
-        await fetch("http://localhost:5050/record", {
+        await fetch("https://hk-badminton-server.onrender.com/record", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
